c1c79e9c0ea5c65463ce3418a3bffcd4ff25e435
snoweye <wccsnow@gmail.com>
2021-09-12 19:54:58 -0400

    More outcome analysis

--- R/psrwe_outana.R
+++ R/psrwe_outana.R
@@ -46,12 +46,7 @@ ps_rwe_outana <- function(dta_psrst) {
     is_ci <- exists("CI", dta_psrst)
     is_infer <- exists("INFER", dta_psrst)
 
-    ## unique names
-    u_group <- sort(unique(dta_psrst$Observed$Group))
-    u_arm <- sort(unique(dta_psrst$Observed$Arm))
-    u_stratum <- sort(dta_psrst$Borrow$Stratum)
-
-    ## study configuration
+    ## analysis configuration
     rst_conf <- list(Method       = dta_psrst$Method,
                      Outcome_type = dta_psrst$Outcome_type)
     if (is_km) {
@@ -59,15 +54,15 @@ ps_rwe_outana <- function(dta_psrst) {
     }
     if (is_ci) {
        rst_conf <- c(rst_conf,
-                     dta_psrst$CI[c("Method_ci",
-                                    "Conf_type",
-                                    "Conf_int")])
+                     CI = dta_psrst$CI[c("Method_ci",
+                                         "Conf_type",
+                                         "Conf_int")])
     }
     if (is_infer) {
        rst_conf <- c(rst_conf,
-                     dta_psrst$INFER[c("Method_infer",
-                                       "Alternative",
-                                       "Mu")])
+                     INFER = dta_psrst$INFER[c("Method_infer",
+                                               "Alternative",
+                                               "Mu")])
     }
 
     ## summary observed
@@ -76,83 +71,116 @@ ps_rwe_outana <- function(dta_psrst) {
         dtype <- data.frame(dtype) %>%
             filter(dta_psrst$pred_tp == T)
     }
-
-    id <- NULL
-    for (g in u_group) {
-        for (a in u_arm) {
-            for (s in u_stratum) {
-                id_tmp <- which(dtype$Group   == g &
-                                dtype$Arm     == a &
-                                dtype$Stratum == s)
-                id <- c(id, id_tmp)
-            }
-        }
-    }
-    for (g in u_group) {
-        for (a in u_arm) {
-            id_tmp <- which(dtype$Group   == g &
-                            dtype$Arm     == a &
-                            dtype$Stratum == "Overall")
-            id <- c(id, id_tmp)
-        }
-    }
-
-    rst_obs <- dtype[id, c("Group", "Arm", "Stratum", "N", "Mean", "StdErr")]
+    rst_obs <- dtype
 
     ## summary estimation
-    dtype <- rbind(dta_psrst[[type]]$Stratum_Estimate,
-                   dta_psrst[[type]]$Overall_Estimate)
+    col_est <- c("Mean", "StdErr")
+    if (is_km) {
+        col_est <- c(col_est, "T")
+    }
+
+    dtype <- rbind(dta_psrst[[type]]$Stratum_Estimate[, col_est],
+                   dta_psrst[[type]]$Overall_Estimate[, col_est])
     if (is_km) {
         dtype <- data.frame(dtype) %>%
             filter(dta_psrst$pred_tp == T)
+        dtype <- dtype[, colnames(dtype) != "T"]
     }
-
     rst_est <- data.frame(Stratum = c(dta_psrst$Borrow$Stratum,
-                                      "Overall"),
-                          Mean    = dtype[, "Mean"],
-                          StdErr  = dtype[, "StdErr"])
+                                      "Overall"))
+    rst_est <- cbind(rst_est, dtype)
 
     if (is_rct) {
-        colnames(rst_est) <- c("Stratum", "Mean_Eff", "StdErr_Eff")
-
-        dtype <- rbind(dta_psrst$Control$Stratum_Estimate,
-                       dta_psrst$Control$Overall_Estimate)
+        dtype <- rbind(dta_psrst$Treatment$Stratum_Estimate[, col_est],
+                       dta_psrst$Treatment$Overall_Estimate[, col_est])
         if (is_km) {
             dtype <- data.frame(dtype) %>%
                 filter(dta_psrst$pred_tp == T)
+            dtype <- dtype[, colnames(dtype) != "T"]
         }
-        rst_est_ctl <- data.frame(Mean_CTL   = dtype[, "Mean"],
-                                  StdErr_CTL = dtype[, "StdErr"])
+        rst_est_trt <- data.frame(Stratum = c(dta_psrst$Borrow$Stratum,
+                                          "Overall"))
+        rst_est_trt <- cbind(rst_est_trt, dtype)
 
-        dtype <- rbind(dta_psrst$Treatment$Stratum_Estimate,
-                       dta_psrst$Treatment$Overall_Estimate)
+        dtype <- rbind(dta_psrst$Control$Stratum_Estimate[, col_est],
+                       dta_psrst$Control$Overall_Estimate[, col_est])
         if (is_km) {
             dtype <- data.frame(dtype) %>%
                 filter(dta_psrst$pred_tp == T)
+            dtype <- dtype[, colnames(dtype) != "T"]
         }
-        rst_est_trt <- data.frame(Mean_TRT   = dtype[, "Mean"],
-                                  StdErr_TRT = dtype[, "StdErr"])
-
-        rst_est <- cbind(rst_est,
-                         rst_est_trt,
-			 rst_est_ctl)
+        rst_est_ctl <- data.frame(Stratum = c(dta_psrst$Borrow$Stratum,
+                                          "Overall"))
+        rst_est_ctl <- cbind(rst_est_ctl, dtype)
     }
 
     ## summary CI results
     if (is_ci) {
+        dtype <- rbind(dta_psrst$CI[[type]]$Stratum_Estimate,
+                       dta_psrst$CI[[type]]$Overall_Estimate)
+        if (is_km) {
+            dtype <- cbind(dtype,
+                           T = c(dta_psrst[[type]]$Stratum_Estimate$T,
+                                 dta_psrst[[type]]$Overall_Estimate$T))
+            dtype <- data.frame(dtype) %>%
+                filter(dta_psrst$pred_tp == T)
+            dtype <- dtype[, colnames(dtype) != "T"]
+        }
+        rst_est <- cbind(rst_est, dtype)
+
+        if (is_rct) {
+            dtype <- rbind(dta_psrst$CI$Treatment$Stratum_Estimate,
+                           dta_psrst$CI$Treatment$Overall_Estimate)
+            if (is_km) {
+                dtype <- cbind(dtype,
+                               T = c(dta_psrst$Treatment$Stratum_Estimate$T,
+                                     dta_psrst$Treatment$Overall_Estimate$T))
+                dtype <- data.frame(dtype) %>%
+                    filter(dta_psrst$pred_tp == T)
+                dtype <- dtype[, colnames(dtype) != "T"]
+            }
+            rst_est_trt <- cbind(rst_est_trt, dtype)
+
+            dtype <- rbind(dta_psrst$CI$Control$Stratum_Estimate,
+                           dta_psrst$CI$Control$Overall_Estimate)
+            if (is_km) {
+                dtype <- cbind(dtype,
+                               T = c(dta_psrst$Control$Stratum_Estimate$T,
+                                     dta_psrst$Control$Overall_Estimate$T))
+                dtype <- data.frame(dtype) %>%
+                    filter(dta_psrst$pred_tp == T)
+                dtype <- dtype[, colnames(dtype) != "T"]
+            }
+            rst_est_ctl <- cbind(rst_est_ctl, dtype)
+        }
     }
 
     ## summary INFR results
     if (is_infer) {
+        dtype <- rbind(dta_psrst$INFER[[type]]$Stratum_InferProb,
+                       dta_psrst$INFER[[type]]$Overall_InferProb)
+        if (is_km) {
+            dtype <- cbind(dtype,
+                           T = c(dta_psrst[[type]]$Stratum_Estimate$T,
+                                 dta_psrst[[type]]$Overall_Estimate$T))
+            dtype <- data.frame(dtype) %>%
+                filter(dta_psrst$pred_tp == T)
+            dtype <- dtype[, colnames(dtype) != "T"]
+        }
+        rst_est <- cbind(rst_est, dtype)
     }
 
 
     ## return
-    rst <- c(rst,
-             list(Observed_Summary       = rst_obs,
-                  Estimates_Summary      = rst_est,
-                  Confidence_Interval    = rst_ci,
-                  Inferential_Statistics = rst_infer))
+    rst <- list(Analysis_Setup  = rst_conf,
+                Observed_Summary  = rst_obs,
+                Analysis_Summary = rst_est)
+
+    if (is_rct) {
+        rst$RCT_Summary <- list(Treatment = rst_est_trt,
+                                Control   = rst_est_ctl)
+    }
+
     class(rst) <- get_rwe_class("OUTANA")
     rst
 }
@@ -173,7 +201,9 @@ ps_rwe_outana <- function(dta_psrst) {
 #' @export
 #'
 print.PS_RWE_RST_OUTANA <- function(x, ...) {
-    print(x)
+    print(x$Analysis_Setup)
+    print(x$Observed_Summary)
+    print(x$Analysis_Summary)
     invisible()
 }
 
