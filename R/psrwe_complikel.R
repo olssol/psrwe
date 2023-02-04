88085f0adaa231957ec2fbcf0a3907ef6dfdcc5d
snoweye <wccsnow@gmail.com>
2023-02-04 18:19:49 -0500

    more stderr_method ignore

--- R/psrwe_complikel.R
+++ R/psrwe_complikel.R
@@ -53,7 +53,7 @@ psrwe_compl <- function(dta_psbor, v_outcome = "Y",
     rst_obs <- get_observed(dta_psbor$data, v_outcome)
 
     ## call estimation
-    if (stderr_method == "jk") {
+    if (stderr_method %in% c("jk", "ignore")) {
         rst <- get_ps_cl_km(dta_psbor, v_outcome = v_outcome,
                             outcome_type = outcome_type,
                             f_stratum = get_cl_stratum,
