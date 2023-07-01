0fa4b019dc1a6c2c145f5f7f8d27e50a2e891a2a
snoweye <wccsnow@gmail.com>
2023-07-01 11:37:59 -0400

    Fix cjk for log-rank and rmst2

--- R/psrwe_tools_lrkrmst.R
+++ R/psrwe_tools_lrkrmst.R
@@ -76,8 +76,8 @@ get_ps_lrk_rmst_sjk <- function(dta_psbor,
                            f_overall_est = f_overall_est, ...)
 
     ## JK overall stderr
-    rstom <- rst$Effect$Overall_Estimate$Mean
-    sdf <- rep(0, length(rstom))
+    rst_om <- rst$Effect$Overall_Estimate$Mean
+    sdf_om <- rep(0, length(rst_om))
 
     n_jk <- nrow(data)
     dta_psbor_jk <- dta_psbor
@@ -87,12 +87,12 @@ get_ps_lrk_rmst_sjk <- function(dta_psbor,
                                   v_event = v_event, v_time = v_time,
                                   f_stratum = f_stratum,
                                   f_overall_est = f_overall_est, ...)
-        sdf <- sdf + (rst_jk$Effect$Overall_Estimate$Mean - rstom)^2
+        sdf_om <- sdf_om + (rst_jk$Effect$Overall_Estimate$Mean - rst_om)^2
     }
 
     ## update rst
     nc_jk <- (n_jk - 1) / n_jk
-    rst$Effect$Overall_Estimate$StdErr <- sqrt(sdf * nc_jk)
+    rst$Effect$Overall_Estimate$StdErr <- sqrt(sdf_om * nc_jk)
 
     ## return
     return(rst)
@@ -116,7 +116,11 @@ get_ps_lrk_rmst_cjk <- function(dta_psbor,
 
     ## prepare data
     data_org <- dta_psbor$data
+    Call_arg <- dta_psbor$Call_arg
     Call_fml <- dta_psbor$Call_fml
+    for (i_call in 1:length(Call_arg)) {
+        Call_arg[[i_call]][[".drop_arg_fml"]] <- TRUE
+    }
 
     ## main estimates
     rst <- get_ps_lrk_rmst(dta_psbor, v_outcome = v_outcome,
@@ -125,35 +129,26 @@ get_ps_lrk_rmst_cjk <- function(dta_psbor,
                            f_overall_est = f_overall_est, ...)
 
     ## Complex JK stderr
-    rstom <- rst$Effect$Overall_Estimate$Mean
-    sdf <- rep(0, length(rstom))
-
+    rst_om <- rst$Effect$Overall_Estimate$Mean
+    sdf_om <- rep(0, length(rst_om))
 
     ## Complex JK
     n_jk <- nrow(data_org)
     n_jk_noerr <- n_jk
 
     for (i_jk in 1:n_jk) {
-        ## Avoid interruptions by errors during PS steps.
         rst_try <- try({
-            ## Get the jk dataset from the original data (without trimming).
-            data_jk <- data_org[-i_jk,]
+            ## get the jk dataset from the original data (without trimming).
+            tmp_rst_jk <- data_org[-i_jk,]
 
-            ## Repeat all saved psrwe steps prior to get_ps_cl_km() call.
-            ## dta_ps <- psrwe_est(data, ...)
-            ## dta_ps_mat <- psrwe_match(dta_ps, ...)
-            ## dta_psbor_jk <- psrwe_match(dta_ps_mat, ...)
-
-            name_rst <- as.character(Call_fml[[1]][[2]])
-            assign(name_rst, data_jk)
-
-            for (i_call in 1:(length(Call_fml) - 1)) {
-              tmp_rst  <- eval(Call_fml[[i_call]])
-              name_rst <- as.character(Call_fml[[i_call + 1]][[2]])
-              assign(name_rst, tmp_rst)
+            ## repeat all saved psrwe steps prior to get_ps_cl_km() call.
+            ## > dta_ps <- psrwe_est(data, ...)
+            ## > dta_ps_mat <- psrwe_match(dta_ps, ...)
+            ## > dta_psbor_jk <- psrwe_match(dta_ps_mat, ...)
+            for (i_call in 1:length(Call_fml)) {
+                Call_arg[[i_call]][[1]] <- tmp_rst_jk
+                tmp_rst_jk <- do.call(Call_fml[[i_call]], Call_arg[[i_call]])
             }
-
-            dta_psbor_jk <- eval(Call_fml[[i_call + 1]])
         }, silent = TRUE)
 
         if (inherits(rst_try, "try-error")) {
@@ -161,16 +156,16 @@ get_ps_lrk_rmst_cjk <- function(dta_psbor,
             next
         }
 
-        rst_jk <- get_ps_lrk_rmst(dta_psbor_jk, v_outcome = v_outcome,
+        rst_jk <- get_ps_lrk_rmst(tmp_rst_jk, v_outcome = v_outcome,
                                   v_event = v_event, v_time = v_time,
                                   f_stratum = f_stratum,
                                   f_overall_est = f_overall_est, ...)
-        sdf <- sdf + (rst_jk$Effect$Overall_Estimate$Mean - rstom)^2
+        sdf_om <- sdf_om + (rst_jk$Effect$Overall_Estimate$Mean - rst_om)^2
     }
 
     ## update rst
     nc_jk <- (n_jk_noerr - 1) / n_jk_noerr
-    rst$Effect$Overall_Estimate$StdErr <- sqrt(sdf * nc_jk)
+    rst$Effect$Overall_Estimate$StdErr <- sqrt(sdf_om * nc_jk)
 
     ## return
     return(rst)
