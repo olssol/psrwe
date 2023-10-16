9442dc896468d7b2640685a2c1fa85c7d10d6248
snoweye <wccsnow@gmail.com>
2023-10-16 17:03:39 -0400

    remove ns5 from sec_5_1_ex

--- demo/sec_5_1_ex.r
+++ demo/sec_5_1_ex.r
@@ -1,4 +1,4 @@
-### Example of Section 4.1.
+### Example of Section 5.1.
 suppressMessages(library(psrwe, quietly = TRUE))
 options(digits = 3)
 data(ex_dta)
@@ -23,14 +23,6 @@ ps_bor_single_ns1 <- psrwe_borrow(dta_ps_single_ns1, total_borrow = 30)
 ### PSPP, single arm study, binary outcome, weights of ATT.
 options(mc.cores = 1)
 .msg <- capture.output({ suppressWarnings({
-rst_pp_ns5 <- psrwe_powerp_watt(ps_bor_single_ns5,
-                            outcome_type = "binary",
-                            v_outcome    = "Y_Bin",
-                            seed         = 1234)
-}) })
-rst_pp_ns5
-
-.msg <- capture.output({ suppressWarnings({
 rst_pp_ns1 <- psrwe_powerp_watt(ps_bor_single_ns1,
                             outcome_type = "binary",
                             v_outcome    = "Y_Bin",
@@ -48,8 +40,6 @@ rst_pp <- psrwe_powerp(ps_bor_single_ns5,
 rst_pp
 
 ### Outcome analysis.
-oa_pp_ns5 <- psrwe_outana(rst_pp_ns5, mu = 0.4)
-oa_pp_ns5
 oa_pp_ns1 <- psrwe_outana(rst_pp_ns1, mu = 0.4)
 oa_pp_ns1
 oa_pp <- psrwe_outana(rst_pp, mu = 0.4)
