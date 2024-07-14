edb5fce6733091dd42eb78752ec4bf7a75f820d8
snoweye <wccsnow@gmail.com>
2024-07-13 23:55:50 -0400

    Add demo for ipw_method

--- demo/sec_6_1_ex.r
+++ demo/sec_6_1_ex.r
@@ -16,6 +16,9 @@ dta_ps_single <- psrwe_est(ex_dta,
 ### Obtain discounting parameters.
 ps_bor_single <- psrwe_borrow(dta_ps_single, total_borrow = 30)
 
+### Obtain discounting parameters (A = 1 and eps in Xi.Ada.Wang).
+ps_bor_xaw <- psrwe_borrow(dta_ps_single, total_borrow = 1)
+
 
 ### PSPP, single arm study, binary outcome, weights of ATT.
 options(mc.cores = 1)
@@ -27,25 +30,7 @@ rst_pp <- psrwe_powerp_watt(ps_bor_single,
 }) })
 rst_pp
 
-### PSPP, single arm study, binary outcome, weights of ATT, analytic solution.
-rst_ppana <- psrwe_powerp_watt(ps_bor_single,
-                               outcome_type = "binary",
-                               v_outcome    = "Y_Bin",
-                               mcmc_method  = "analytic",
-                               seed         = 1234)
-rst_ppana
-
-### Outcome analysis.
-oa_pp <- psrwe_outana(rst_pp, mu = 0.4)
-oa_pp
-oa_ppana <- psrwe_outana(rst_ppana, mu = 0.4)
-oa_ppana
-
-
-### Obtain discounting parameters (A = 1 and eps in Xi.Ada.Wang).
-ps_bor_xaw <- psrwe_borrow(dta_ps_single, total_borrow = 1)
-
-### PSPP, single arm study, binary outcome, weights of ATT, Xi.Ada.Wang
+### PSPP, single arm study, binary outcome, weights of ATT, Xi.Ada.Wang.
 options(mc.cores = 1)
 .msg <- capture.output({ suppressWarnings({
 rst_xaw <- psrwe_powerp_watt(ps_bor_xaw,
@@ -56,6 +41,8 @@ rst_xaw <- psrwe_powerp_watt(ps_bor_xaw,
 rst_xaw
 
 ### Outcome analysis.
+oa_pp <- psrwe_outana(rst_pp, mu = 0.4)
+oa_pp
 oa_xaw <- psrwe_outana(rst_xaw, mu = 0.4)
 oa_xaw
 
